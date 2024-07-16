e1bfcb6c051beee722324bd77e066f3de8450ee9
Cuong Nguyen <cuongnguyen29696@gmail.com>
2024-07-16 21:05:21 +0700

    Add MainWindow

--- src/shared/constants/environment.ts
+++ src/shared/constants/environment.ts
@@ -1,5 +1,6 @@
 export const ENVIRONMENT = {
   IS_DEV: import.meta.env.DEV,
+  IS_LOCAL: import.meta.env.MODE === 'local_dev',
   IS_DEBUG: !import.meta.env.PROD,
   IS_PRODUCTION: import.meta.env.PROD,
 };
